85ee2322479c5fc36eea0c651a02b500df857daf
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-10-19 14:01:55 +0200

    fixed selcet level

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -1,10 +1,5 @@
-/*Consegna*
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+/*Bonus
 
-Bonus
 Solo se l'esercizio base funziona perfettamente: create una nuova cartella chiamata bonus e copiateci dentro tutti i files e cartelle dell'esercizio base tranne la cartella .git.
 Poi procedete ad implementare il bonus come segue.
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
@@ -18,58 +13,55 @@ Di cosa ho bisogno per generare la griglia
 Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 Le validazioni e i controlli possiamo farli anche in un secondo momento.*/
 
-const btngame = document.querySelector('button')
-btngame.addEventListener('click', campoMinato)
 
-function campoMinato(){
-    // mi prendo il campo da gioco
-    const playground = document.querySelector('.playground')
-    // controllo che posso il reset del pulsante di invio
-    playground.innerHTML = '';
+campoMinatoGame();
 
-    // mi prendo la selcet
-    const level = document.getElementById('level')
-    // mi prendo il valore della select
-    const levelType = level.value;
+function campoMinatoGame() {
+    const btngame = document.querySelector('button');
+    btngame.addEventListener('click', campoMinato);
 
-    // numero quadratini da generare
-    let numSquare;
+    function campoMinato() {
+        const level = document.getElementById('level').value;
+        const numSquare = selectLevel(level);
 
-    // condizione if 
-    if (levelType === 1){
-        numSquare = 100;
-    }
-    if (levelType === 2){
-        numSquare = 81;
-    }
-    if (levelType === 3){
-        numSquare = 49;
+        const playground = document.querySelector('.playground');
+        playground.innerHTML = '';
+
+        for (let i = 0; i < numSquare; i++) {
+            const square = createSquare(i, numSquare);
+            playground.appendChild(square);
+        }
     }
 
-    // mi creo il ciclo per i quadratini    
-    for(let i = 0; i < numSquare; i++){
-        // genero i quadratini
-        let square = createSquare (i);
-        // appendo i quadratini nel playground
-        playground.append(square)
+    function createSquare(squareInd, numSquare) {
+        const squareWidth = Math.sqrt(numSquare);
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.innerHTML = squareInd + 1;
+        square.style.width = `calc(100% / ${squareWidth})`;
+        square.style.height = square.style.width;
+
+        square.addEventListener('click', function () {
+            square.classList.add('squarenew');
+            square.style.color = 'black';
+            console.log(`Quadratino cliccato: ${squareInd + 1}`);
+        });
+
+        return square;
     }
-}
-    
 
-
-// funzione per creare i quadratini
-
-function createSquare (squareInd){
-    // dove 
-    const square = document.createElement('div');
-    // aggiungo classe
-    square.classList.add('square')
-    // aggiungo numero indice
-    square.innerHTML = squareInd + 1 ;
-    square.addEventListener('click', function(){
-        square.classList.add('squarenew')
-        square.style.color = 'black'
-        console.log(`Quadratino cliccato: ${squareInd + 1}`)
-    })
-    return square;
+    function selectLevel(level) {
+        let numSquare;
+        switch (level) {
+            case 'medio':
+                numSquare = 81;
+                break;
+            case 'difficile':
+                numSquare = 49;
+                break;
+            default:
+                numSquare = 100;
+        }
+        return numSquare;
+    }
 }
